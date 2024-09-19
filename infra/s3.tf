f8fb316e30c601a906d92a6e8e0d5f10d7a89708
K-Kizuku <kotani.kizuku@gmail.com>
2024-09-19 13:41:19 +0900

    feat: 🍻 terraform frontend server

---
+++ infra/s3.tf
@@ -0,0 +1,39 @@
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket_prefix = "static-www"
+
+
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_bucket_website" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket" {
+    bucket = aws_s3_bucket.frontend_bucket.id
+    policy = data.aws_iam_policy_document.static-www.json
+}
+
+data "aws_iam_policy_document" "static-www" {
+  statement {
+    sid = "Allow CloudFront"
+    effect = "Allow"
+    principals {
+        type = "AWS"
+        identifiers = [aws_cloudfront_origin_access_identity.static-www.iam_arn]
+    }
+    actions = [
+        "s3:GetObject"
+    ]
+
+    resources = [
+        "${aws_s3_bucket.frontend_bucket.arn}/*"
+    ]
+  }
+}
+
+output "S3_URI" {
+  value = aws_s3_bucket.frontend_bucket.website_endpoint
+}
